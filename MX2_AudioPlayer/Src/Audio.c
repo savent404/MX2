ed95bd0abf3161d8d77e2690b004e0bdb38e035a
Liao Yuankai <savent_gate@outlook.com>
2017-07-13 23:35:49 +0800

    修复触发优先级的错误

--- MX2_AudioPlayer/Src/Audio.c
+++ MX2_AudioPlayer/Src/Audio.c
@@ -27,8 +27,8 @@ extern osMessageQId DAC_CMDHandle;
 extern osMessageQId LED_CMDHandle;
 extern osSemaphoreId DAC_Complete_FlagHandle;
 
-#define PRI_TRIGGER_B           2
-#define PRI_TRIGGER_C           3
+#define PRI_TRIGGER_B           3
+#define PRI_TRIGGER_C           2
 #define PRI_TRIGGER_D           4
 #define PRI_TRIGGER_E           1
 #define PRI_TRIGGER_NULL        0x0F
