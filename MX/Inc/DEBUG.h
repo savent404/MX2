f998a5e8c8375addb6f9ad06b1d039b14c258d6c
savent_destop_win10 <savent_gate@outlook.com>
2017-10-11 00:15:33 +0800

    加入debug文件

--- MX/Inc/DEBUG.h
+++ MX/Inc/DEBUG.h
@@ -1,6 +1,7 @@
 #ifndef _DEBUG_H_
 #define _DEBUG_H_
 
+#include "elog.h"
 #include "mx-config.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -20,7 +21,15 @@ int EBmonitor_kbhit();
 #define EBmonitor_Init()                                        \
   {                                                             \
     EBmonitor_buffer(stdout, EBmonitorBuffer, EBmonitorBufLen); \
-  }
+    elog_init();                                                \
+    elog_start();                                               \
+    elog_set_fmt(ELOG_LVL_ASSERT, ELOG_FMT_ALL);                \
+    elog_set_fmt(ELOG_LVL_WARN, ELOG_FMT_ALL);                  \
+    elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_ALL);                  \
+    elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_ALL);                 \
+    elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_ALL);               \
+  \
+}
 #define DEBUG(level, format, ...)                                                                   \
   {                                                                                                 \
     if (level < 10)                                                                                 \
