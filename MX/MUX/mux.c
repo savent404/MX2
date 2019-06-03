savent404/MX2

commit a37391200265deba86db17c523a9d1c0e7691217

优化mux合成音效
see !64

MX/MUX/mux.c
@@ -190,6 +190,23 @@ void MX_MUX_Handle(void const* arg)
         memset(storageBuffer, 0, sizeof(int) * bufferSize);
 
         waitMutex(pTrack->id);
+
+        // calculate public vol
+        int vol_ans = 0;
+        for (int i = 0; i < pTrack->maxium_slot; i++) {
+            pSlot = &pTrack->slots[ i ];
+            if (pSlot->mode == SlotMode_Idle)
+                continue;
+            vol_ans += pSlot->vol;
+            if ((int)(pTrack->id) == 2) {
+                DEBUG(5, "%02d:%02d", i, pSlot->vol);
+            }
+        }
+        if (vol_ans <= 0) {
+            vol_ans = 1; // just keep div right
+        }
+        float volMulti = (float)(USR.config->Vol) / (float)(vol_ans);
+
         for (int i = 0; i < pTrack->maxium_slot; i++) {
             pSlot = &pTrack->slots[ i ];
 
@@ -200,9 +217,15 @@ void MX_MUX_Handle(void const* arg)
             int leftSize = mux_fileObj_getSize(pSlot->pObj) - mux_fileObj_tell(pSlot->pObj);
             int readedSize;
             if (leftSize >= bufferByteSize) {
-                readedSize = mux_fileObj_read(pSlot->pObj, readBuffer, bufferByteSize);
-                leftSize   = bufferByteSize - readedSize;
-                mux_convert_addToInt(readBuffer, storageBuffer, bufferSize, &f, pSlot->vol);
+                if (pSlot->vol) {
+                    readedSize = mux_fileObj_read(pSlot->pObj, readBuffer, bufferByteSize);
+                    leftSize   = bufferByteSize - readedSize;
+                    mux_convert_addToInt(readBuffer, storageBuffer, bufferSize, volMulti, &f, pSlot->vol);
+                } else {
+                    int offset = mux_fileObj_tell(pSlot->pObj) + bufferByteSize;
+                    mux_fileObj_seek(pSlot->pObj, offset);
+                    continue;
+                }
             }
 
             if (!leftSize)

MX/LOOP/LOOP.cpp
@@ -428,7 +428,7 @@ void handleRunning(void)
         USR.config->SwingThreshold_L = 1;
 
     t     = t < 0 ? -t : t;
-    sGyro = 0.5f * sGyro + 0.49f * t;
+    sGyro = 0.7f * sGyro + 0.29f * t;
     if (isinf(sGyro))
         sGyro = 0;
     if (isnan(sGyro))

MX/MUX/mux_convert_internal.h
@@ -8,7 +8,7 @@
 #endif
 
 MX_C_API void
-mux_convert_addToInt(const void* source, int* dest, int size, float* f, int multiFactor);
+mux_convert_addToInt(const void* source, int* dest, int size, const float absolutMulti, float* f, int multiFactor);
 
 MX_C_API void
 mux_convert_mergeToBuffer(const int* source, void* dest, int size, int shiftBits);

MX/MUX/mux_convert_internal_dac.c
@@ -1,13 +1,13 @@
 #include "mux_convert_internal.h"
 
 MX_C_API void
-mux_convert_addToInt(const void* source, int* dest, int size, float* f, int multiFactor)
+mux_convert_addToInt(const void* source, int* dest, int size, const float absolutMulti, float* f, int multiFactor)
 {
     static const float factor = 64;
     static const float div    = 2;
     static const int   top    = INT16_MAX * (1 << MX_MUX_WAV_VOL_LEVEL);
     static const int   button = INT16_MIN * (1 << MX_MUX_WAV_VOL_LEVEL);
-    float              _f     = *f;
+    float              _f     = *f * absolutMulti;
     int                buf;
     int16_t*           _s = (int16_t*)source;
     int*               _d = dest;
@@ -24,7 +24,7 @@ mux_convert_addToInt(const void* source, int* dest, int size, float* f, int mult
         }
         *_d++ = *_d + (*_s) * multiFactor * _f;
     }
-    *f = _f;
+    *f = _f / absolutMulti;
 }
 
 MX_C_API void

commit fc8ac438766a1aed8ee1e7eede5fae0931afbaff

fix problems introduced in a3739120(see !64)
- skip the unused chunk in file to support more user's file
- fix some usage of vars
- optimize the ops of mux handle loop

MX/MUX/mux.c
@@ -218,21 +218,18 @@ void MX_MUX_Handle(void const* arg)
                 continue;
             /** pSlot->mode != SlotMode_Idel ***/
 
-            int readedSize = 0;
+            int  readedSize = 0;
+            int  tmpReadSize;
             int* pStoraged = storageBuffer;
-            int leftSize;
-            int bytesNeeded;
-readAgain:
-            leftSize   = mux_wavOps_getSize(pSlot->pObj) - mux_wavOps_tell(pSlot->pObj);
+            int  leftSize;
+            int  bytesNeeded;
+        readAgain:
+            tmpReadSize = 0;
+            leftSize    = mux_wavOps_getSize(pSlot->pObj) - mux_wavOps_tell(pSlot->pObj);
             bytesNeeded = bufferByteSize - readedSize;
-            if (leftSize >= bytesNeeded) {
-                readedSize += mux_wavOps_read(pSlot->pObj, readBuffer, bytesNeeded);
-            } else if (leftSize) {
-                readedSize += mux_wavOps_read(pSlot->pObj, readBuffer, leftSize);
-            } else {
-                // FILE EOF
-            }
-            mux_convert_addToInt(readBuffer, pStoraged, readedSize / sizeof(muxBuffer_t), volMulti, &f, pSlot->vol);
+            tmpReadSize = mux_wavOps_read(pSlot->pObj, readBuffer, leftSize >= bytesNeeded ? bytesNeeded : leftSize);
+            readedSize += tmpReadSize;
+            mux_convert_addToInt(readBuffer, pStoraged, tmpReadSize / sizeof(muxBuffer_t), volMulti, &f, pSlot->vol);
 
             // if no read error then skip next ops
             // if no read ops or read error, goto clear stage

MX/MUX/mux_wavops.c
@@ -14,6 +14,18 @@ static inline uint16_t readU16FromBuffer(char* arry, int offset)
 {
     return *(uint16_t*)(arry + offset);
 }
+// check if file got a useless chunk
+static inline bool matchedUnknowChunk(char* ptrChunk) {
+    const char list[][5] = {
+        "LIST",
+    };
+
+    for (int i = 0; i < sizeof(list) / sizeof(list[0]); i++) {
+        if (!strncmp(ptrChunk, list[i], 4))
+            return true;
+    }
+    return false;
+}
 
 MX_C_API bool
 mux_wavOps_open(MUX_FileObj_t* ptr, const char* filePath)
@@ -34,14 +46,35 @@ mux_wavOps_open(MUX_FileObj_t* ptr, const char* filePath)
         goto openFaild;
     if (readU16FromBuffer(chunkBuffer, 20) != 1)
         goto openFaild;
-    if (strncmp(chunkBuffer + 36, "data", 4))
-        goto openFaild;
+
     ptr->info.channels      = readU16FromBuffer(chunkBuffer, 22);
     ptr->info.samplesPreSec = readU32FromBuffer(chunkBuffer, 24);
     ptr->info.blockAlign    = readU16FromBuffer(chunkBuffer, 32);
     ptr->info.bitsPreSample = readU16FromBuffer(chunkBuffer, 34);
-    ptr->info.dataOffset    = MX_MUX_WAV_FIX_OFFSET;
-    ptr->info.dataSize      = readU32FromBuffer(chunkBuffer, 40);
+
+    int tmp = 36;
+    int offset = 36;
+    while (strncmp(chunkBuffer + tmp, "data", 4)) {
+
+        // if is a unacceptable chunk, just go faild
+        if (!matchedUnknowChunk(chunkBuffer + tmp)) {
+            goto openFaild;
+        }
+
+        // read this chunk's size, skip to the next chunk for 'data'
+        offset += readU32FromBuffer(chunkBuffer, tmp + 4) + 8;
+
+        // if error at once, tmp will be reset at all the time
+        tmp = 0;
+
+
+        if (!mux_fileObj_seek(&ptr->fileObj, offset))
+            goto openFaild;
+        if (!mux_fileObj_read(&ptr->fileObj, chunkBuffer, 8))
+            goto openFaild;
+    }
+    ptr->info.dataOffset = offset + 8;
+    ptr->info.dataSize = readU32FromBuffer(chunkBuffer, tmp + 4);
 
     MUX_OPS_DEBUG(5, "Info:");
     MUX_OPS_DEBUG(5, "\tchannels:%u", ptr->info.channels);